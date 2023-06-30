52048ca80019958f09d6e4f78d91de93c9109ce5
PaoloVeliz <paoloveliz@gmail.com>
2023-06-29 22:27:16 -0600

    add jar file

--- Services/api_temperature/api/app.py
+++ Services/api_temperature/api/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from service import Heat
-app = Flask(__name__)
+from flask_cors import CORS
 
-@app.route('/allAges/', methods=['GET', 'POST'])
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/allAges/', methods=['GET'])
 def welcome():
     return Heat.getAllData()
+
+@app.route('/holi', methods=['GET'])
+def hello():
+    return "holi"
+
 
 if __name__ == '__main__':
     app.run()
